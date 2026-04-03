734cb91b1b5dbde467351e40956f7bb292f5d5cb
amanraina1 <amanraina9086@gmail.com>
2026-04-03 22:47:03 +0530

    fresh start

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue';
-
-// bootstrap css/js
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'
-
-createApp(App).mount('#app')
